ce5623476994f66eeea79ca5e105a11b1e004551
Harshitha.Yeddula <mail2harsh256@gmail.com>
2019-08-23 15:59:42 +0530

    First commit

---
+++ src/main/java/com/philips/casestudy2/phase1/SpringalertingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.philips.casestudy2.phase1.Springalertingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringalertingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringalertingsystemApplication.class, args);
+	}
+
+}
