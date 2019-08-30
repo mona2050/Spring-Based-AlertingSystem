2ec379315bffc91e9db5acbb87288b4ec3d836f0
mayankphilips <mayank.jaguri@philips.com>
2019-08-30 15:44:13 +0530

    Adding The Test Files

---
+++ src/test/java/com/philips/casestudy2/springalertingsystem/service/TestOxygenConcentration.java
@@ -0,0 +1,85 @@
+/*
+ * The copyright of this file belongs to Koninklijke Philips N.V., 2019.
+ */
+package com.philips.casestudy2.springalertingsystem.service;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestOxygenConcentration {
+  VitalCheck test;
+
+
+
+  @Test
+  public void testOxygenForNormalLevelLowerBound1() {
+
+
+    final int result = test.oxygenCheck(91);
+    assertEquals(0,result);
+  }
+
+  @Test
+  public void testOxygenForNormalLevelLowerBound2() {
+
+    final int result = test.oxygenCheck(92);
+    assertEquals(0,result);
+  }
+
+  @Test
+  public void testOxygenForNormalLevelUpperBound1() {
+
+
+    final int result = test.oxygenCheck(99);
+    assertEquals(0,result);
+  }
+
+  @Test
+  public void testOxygenForNormalLevelUpperBound2() {
+
+
+    final int result = test.oxygenCheck(100);
+    assertEquals(0,result);
+  }
+
+
+
+  @Test
+  public void testOxygenForLowLevelLowerBound1() {
+
+    final int result = test.oxygenCheck(69);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testOxygenForLowLevelLowerBound2() {
+
+    final int result = test.oxygenCheck(70);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testOxygenForLowLevelLowerBound3() {
+
+
+    final int result = test.oxygenCheck(71);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testOxygenForLowLevelUpperBound1() {
+
+
+    final int result = test.oxygenCheck(89);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testOxygenForLowLevelUpperBound2() {
+
+
+    final int result = test.oxygenCheck(90);
+    assertEquals(1,result);
+  }
+
+
+}
---
+++ src/test/java/com/philips/casestudy2/springalertingsystem/service/TestTemprature.java
@@ -0,0 +1,120 @@
+/*
+ * The copyright of this file belongs to Koninklijke Philips N.V., 2019.
+ */
+package com.philips.casestudy2.springalertingsystem.service;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestTemprature {
+  VitalCheck test ;
+
+  @Test
+  public void testTempratureNormal1() {
+    final int result = test.temperatureCheck(97.00);
+    assertEquals(0,result);
+  }
+
+
+  @Test
+  public void testTempratureNormal2() {
+    final int result = test.temperatureCheck(97.01);
+    assertEquals(0,result);
+  }
+
+  @Test
+  public void testTempratureNormal3() {
+    final int result = test.temperatureCheck(98.99);
+    assertEquals(0,result);
+  }
+
+  @Test
+  public void testTempratureNormal4() {
+    final int result = test.temperatureCheck(99.00);
+    assertEquals(0,result);
+  }
+
+  @Test
+  public void testTempratureLow1() {
+    final int result = test.temperatureCheck(92.99);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureLow2() {
+    final int result = test.temperatureCheck(93.00);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureLow3() {
+
+
+
+    final int result = test.temperatureCheck(93.01);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureLow4() {
+
+
+
+    final int result = test.temperatureCheck(96.98);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureLow5() {
+
+
+
+    final int result = test.temperatureCheck(96.99);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureHigh1() {
+
+
+
+    final int result = test.temperatureCheck(99.01);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureHigh2() {
+
+
+
+    final int result = test.temperatureCheck(99.02);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureHigh3() {
+
+
+
+    final int result = test.temperatureCheck(107.98);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureHigh4() {
+
+
+
+    final int result = test.temperatureCheck(107.99);
+    assertEquals(1,result);
+  }
+
+  @Test
+  public void testTempratureHigh5() {
+
+
+
+    final int result = test.temperatureCheck(108.00);
+    assertEquals(1,result);
+  }
+
+}
